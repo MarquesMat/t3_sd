a521e95e0c169e27e14400c588c6884efca4109e
MarquesMat <90152478+MarquesMat@users.noreply.github.com>
2023-11-28 15:38:31 -0300

    Update app.js
    Adaptando para chamar os novos módulos

--- server/app.js
+++ server/app.js
@@ -12,6 +12,10 @@ const express = require('express');
 const mysql = require('mysql');
 const fs = require('fs'); // Ler o arquivo json com as credenciais
 
+const apiGet = require('./get.js');
+const apiDelete = require('./delete.js');
+const apiPost = require('./post.js');
+
 const app = express();
 const port = 3000;
 
@@ -50,74 +54,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Rotas para obter dados das tabelas (GET)
-// Rota para obter dados da tabela film
-app.get('/filmes', (req, res) => {
-  db.query('SELECT * FROM film', (err, results) => {
-    if (err) {
-      res.status(500).send('Erro ao consultar o banco de dados');
-    } else {
-      res.json(results);
-    }
-  });
-});
+app.use('/', apiGet);
 
-// Rota para obter dados da tabela actor
-app.get('/atores', (req, res) => {
-  db.query('SELECT * FROM actor', (err, results) => {
-    if (err) {
-      res.status(500).send('Erro ao consultar o banco de dados');
-    } else {
-      res.json(results);
-    }
-  });
-});
+// Rotas para adicionar novas instâncias nas tabelas (POST)
+app.use('/', apiPost);
 
-// Rota para obter dados da tabela category
-app.get('/categorias', (req, res) => {
-  db.query('SELECT * FROM category', (err, results) => {
-    if (err) {
-      res.status(500).send('Erro ao consultar o banco de dados');
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-// Rotas para adicionar novas instâncias nas tabelas
-// Rota para adicionar um novo filme (POST)
-app.post('/filmes', (req, res) => {
-  const { title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features } = req.body;
-
-  const query = `
-    INSERT INTO film (title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-  `;
-
-  const values = [title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features];
-
-  db.query(query, values, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: 'Erro ao adicionar filme ao banco de dados' });
-    } else {
-      res.json({ success: true, message: 'Filme adicionado com sucesso' });
-    }
-  });
-});
-
-// Rota para excluir um filme pelo ID (DELETE)
-app.delete('/filmes/:id', (req, res) => {
-  const filmeId = req.params.id;
-
-  const query = 'DELETE FROM film WHERE film_id = ?';
-
-  db.query(query, [filmeId], (err, results) => {
-    if (err) {
-      res.status(500).json({ error: 'Erro ao excluir filme do banco de dados' });
-    } else {
-      res.json({ success: true, message: 'Filme excluído com sucesso' });
-    }
-  });
-});
+// Rotas para deletar instâncias das tabelas pelo ID (DELETE)
+app.use('/', apiDelete);
 
 // Iniciar o servidor
 app.listen(port, () => {
